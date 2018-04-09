115810a73ba36a82759b0f0f0c35ac14ebc82e2d
Harsh Tomar <harshtomar6@gmail.com>
2018-04-09 13:24:07 +0530

    Added Events

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DATABASE_URI: 'mongodb://harshtomar6:bjn721@ds123695.mlab.com:23695/clip',
+  DATABASE_URI: 'mongodb://harshtomar6:bjn721@ds159997.mlab.com:59997/verve-campaign',
   PORT: process.env.PORT || 3000
 }
